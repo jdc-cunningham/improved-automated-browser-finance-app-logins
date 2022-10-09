b2fb6f7dd3ccf0c06484d5c90c13698a17a647db
Jacob Cunningham <jcun4128@gmail.com>
2022-10-09 11:26:51 -0500

    working dynamic fields and localStorage

--- chrome-extension/popup-ui/index.js
+++ chrome-extension/popup-ui/index.js
@@ -1,5 +1,7 @@
 // the end goal of this interface is to produce a JSON interaction structure
 // like sample-account.json
+// interactions is an array but here it's an object due to the id key
+// on save it is turned into an array by order of interaction creation
 
 // temporarily store data until done, then inserted into db
 let storage = localStorage.getItem('ibfa-temp-store');
@@ -12,42 +14,110 @@ const addInteraction = document.getElementById('add-interaction');
 const addedInteractions = document.getElementById('added-interactions');
 const addAccount = document.getElementById('add-account');
 
+// globally bounded event for future elements
+// this captures modifications to input elements primarily for interactions
+// forms the object and saves, keeps order of interactions
+document.addEventListener('keyup', (el) => {
+  const parentId = el.target.parentNode.parentNode.id;
+  const interactionType = document.getElementById(parentId).getAttribute('data-type');
+  console.log('parent id', parentId);
+
+  if (!('interactions' in storage)) {
+    storage['interactions'] = {};
+  }
+
+  storage['interactions'][parentId] = {
+    id: parentId,
+    type: interactionType
+  };
+
+  switch (interactionType) {
+    case "input":
+      Array.from(document.getElementById(parentId).getElementsByTagName('INPUT')).forEach(input => {
+        storage['interactions'][parentId][input.name] = input.value;
+      });
+      break;
+    case "button":
+      Array.from(document.getElementById(parentId).getElementsByTagName('INPUT')).forEach(input => {
+        storage['interactions'][parentId][input.name] = input.value;
+      });
+      break;
+    case "2fa option":
+      Array.from(document.getElementById(parentId).getElementsByTagName('INPUT')).forEach(input => {
+        storage['interactions'][parentId][input.name] = input.value;
+      });
+      break;
+    case "2fa input":
+      Array.from(document.getElementById(parentId).getElementsByTagName('INPUT')).forEach(input => {
+        storage['interactions'][parentId][input.name] = input.value;
+      });
+      break;
+    case "balance target":
+      Array.from(document.getElementById(parentId).getElementsByTagName('INPUT')).forEach(input => {
+        storage['interactions'][parentId][input.name] = input.value;
+      });
+      break;
+    default:
+      // do nothing
+      break;
+  }
+
+  saveData();
+});
+
+document.addEventListener('click', (el) => {
+  console.log(el);
+  if (el.target.nodeName === "BUTTON" && el.target.name === "remove") {
+    const parentId = el.target.parentNode.id;
+    console.log(parentId, storage);
+    delete storage.interactions[parentId];
+    document.getElementById(parentId).remove();
+    console.log(storage);
+    saveData();
+  }
+});
+
 const renderHtml = (interaction) => {
   console.log(interaction);
   switch (interaction.type) {
     case "input":
-      return `<div class="interaction-group">
+      return `<div id="${interaction.id || Date.now()}" data-type="input" class="interaction-group">
         <p class="bold">type: ${interaction.type || ""}</p>
-        <span>name: <input type="text" value="${interaction.name || ""}"/></span>
+        <span>name: <input type="text" name="name" value="${interaction.name || ""}"/></span>
         <span>dom target: <input type="text" name="dom_target" value="${interaction.dom_target || ""}"/></span>
         <span>value lookup: <input type="text" name="value_lookup" value="${interaction.value_lookup || ""}"/></span>
+        <button name="remove" type="button">remove</button>
       </div>`; // minor duplicate wrapper code
     case "button":
-      return `<div class="interaction-group">
+      return `<div id="${Date.now()}" data-type="button" class="interaction-group">
         <p class="bold">type: ${interaction.type || ""}</p>
-        <span>name: <input type="text" value="${interaction.name || ""}"/></span>
+        <span>name: <input type="text" name="name" value="${interaction.name || ""}"/></span>
         <span>dom target: <input type="text" name="dom_target" value="${interaction.dom_target || ""}"/></span>
+        <button name="remove" type="button">remove</button>
       </div>`;
     case "2fa option":
       // url is not really important here but can be used
       // there is also the possiblity of multi select issue that's problematic on puppeteer side
-      return `<div class="interaction-group">
+      return `<div id="${Date.now()}" data-type="2fa option" class="interaction-group">
         <p class="bold">type: ${interaction.type || ""}</p>
-        <span>url: <input type="text" value="${interaction.url || ""}"/></span>
+        <span>url: <input type="text" name="url" value="${interaction.url || ""}"/></span>
         <span>dom target: <input type="text" name="dom_target" value="${interaction.dom_target || ""}"/></span>
+        <button name="remove" type="button">remove</button>
       </div>`;
     case "2fa input":
-      return `<div class="interaction-group">
+      return `<div id="${Date.now()}" data-type="2fa input" class="interaction-group">
         <p class="bold">type: ${interaction.type || ""}</p>
-        <span>name: <input type="text" value="${interaction.name || ""}"/></span>
+        <span>name: <input type="text" name="name" value="${interaction.name || ""}"/></span>
         <span>dom target: <input type="text" name="dom_target" value="${interaction.dom_target || ""}"/></span>
         <span>2fa lookup: <input type="text" name="2fa_lookup" value="${interaction.value_lookup || ""}"/></span>
+        <button name="remove" type="button">remove</button>
       </div>`;
     case "balance target":
-      return `<div class="interaction-group">
+      return `<div id="${Date.now()}" data-type="balance target" class="interaction-group">
         <p class="bold">type: ${interaction.type || ""}</p>
         <span>dom target: <input type="text" name="dom_target" value="${interaction.dom_target || ""}"/></span>
         <span>spreadsheet column: <input type="text" name="2fa_lookup" value="${interaction.spreadsheet_column || ""}"/></span>
+        <button name="remove" type="button">remove</button>
       </div>`;
     default:
       return '<p class="bold">Unknown type</p>';
@@ -61,8 +131,8 @@ const loadData = () => {
         accountUrl.value = storage[key];
       } else if (key === 'interactions') {
         // assumes only interactions
-        storage['interactions'].forEach(interaction => {
-          addedInteractions.innerHTML += renderHtml(interaction); // dangerous eg. XSS
+        Object.keys(storage.interactions).forEach(interactionId => {
+          addedInteractions.innerHTML += renderHtml(storage.interactions[interactionId]); // dangerous eg. XSS
         });
       }
     });
@@ -84,7 +154,9 @@ accountUrl.addEventListener('paste', (e) => {
 
 addInteraction.addEventListener('click', () => {
   const selectedInteraction = selectInteraction.value;
+
   addedInteractions.innerHTML += renderHtml({ // jank object due to shared renderer
     type: selectedInteraction
   });
 });
+
